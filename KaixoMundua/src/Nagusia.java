8d0f15976352adfa51f98db8bbb29cac443f44fb
scpirica <scpirica@ehu.es>
2014-03-25 09:33:56 +0100

    Update Nagusia.java
    Hau kanpoan egina

--- KaixoMundua/src/Nagusia.java
+++ KaixoMundua/src/Nagusia.java
@@ -1,8 +1,8 @@
-
 public class Nagusia {
 public static void main(String[] args){
 	// TODO Auto-generated method stub
 System.out.println("Kaixo");
 System.out.println("Beste aldaketa bat ondo baderitzozu");
+System.out.println("Aldaketa hau goiko zerbitzarian egin da");
 }
 }
