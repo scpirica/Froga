025c51b7f62d79c33cd371fe1cc2aca0cb69de4c
scpirica <scpirica@ehu.es>
2014-03-25 09:47:50 +0100

    Update Nagusia.java

--- KaixoMundua/src/Nagusia.java
+++ KaixoMundua/src/Nagusia.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 	// TODO Auto-generated method stub
 System.out.println("Kaixo");
 System.out.println("Beste aldaketa bat ondo baderitzozu");
+//zerbitzarikoa
 }
 }
