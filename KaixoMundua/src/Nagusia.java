b6879c2414560f3da0dba0ba418aa6e89448f772
Ander <scpirica@ehu.es>
2014-03-25 09:32:46 +0100

    Hirugarrena

--- KaixoMundua/src/Nagusia.java
+++ KaixoMundua/src/Nagusia.java
@@ -3,5 +3,6 @@ public class Nagusia {
 public static void main(String[] args){
 	// TODO Auto-generated method stub
 System.out.println("Kaixo");
+System.out.println("Beste aldaketa bat ondo baderitzozu");
 }
 }
